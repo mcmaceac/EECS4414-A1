1c33dba0c20ecafadfbf3d5c146fbf94d91f1675
Matthew MacEachern <mcm.maceachern@gmail.com>
2018-02-20 15:28:24 -0500

    B part iv implemented

--- er.py
+++ er.py
@@ -1,7 +1,19 @@
 import networkx as nx 
 import matplotlib.pyplot as plot 
+import numpy as np
 import sys 
 import re
+import collections
+
+def extract_length_distribution(dict):
+	#this function takes the list of dictionaries from the shortest_path_length function
+	#and extracts the path lengths to return a dictionary of the number of shortest path lengths
+	#where the key is the path length
+	result = []
+	for dictionary in dict:
+		for length in dictionary.values():
+			result.append(length)
+	return collections.Counter(result)
 
 n = 1000
 p = 1/6
@@ -25,3 +37,15 @@ plot.show()
 
 #B part iii
 print("Global clustering coefficient: %s" % nx.average_clustering(G))
+
+#B part iv
+shortestPathDicts = list(nx.shortest_path_length(G).values())
+length_dist = extract_length_distribution(shortestPathDicts)
+x = list(length_dist.keys())
+y = list(length_dist.values())
+plot.xlabel('Shortest Path Length')
+plot.ylabel('Number of shortest paths')
+x_pos = np.arange(len(x))
+plot.bar(x_pos, y, align='center', alpha=0.5)
+plot.xticks(x_pos, x)
+plot.show()
--- test.py
+++ test.py
@@ -1,25 +1,44 @@
 import networkx as nx 
 import matplotlib.pyplot as plt 
+import numpy as np
 import sys 
 import re
 import collections
 
-def extract_lengths(dict):
+def extract_length_distribution(dict):
+	#this function takes the list of dictionaries from the shortest_path_length function
+	#and extracts the path lengths to return a dictionary of the number of shortest path lengths
+	#where the key is the path length
 	result = []
 	for dictionary in dict:
 		for length in dictionary.values():
 			result.append(length)
-	return result
+	return collections.Counter(result)
 
-n = 5
-p = 1/6
+n = 1000
+p = 1/60
 G = nx.erdos_renyi_graph(n, p)
 
 shortestPathDicts = list(nx.shortest_path_length(G).values())
 print(shortestPathDicts)
-print(extract_lengths(shortestPathDicts))
+length_dist = extract_length_distribution(shortestPathDicts)
+
+print(length_dist)
+print(length_dist.keys())
+print(length_dist.values())
+
+x = list(length_dist.keys())
+y = list(length_dist.values())
+
+x_pos = np.arange(len(x))
+plt.bar(x_pos, y, align='center', alpha=0.5)
+plt.xticks(x_pos, x)
+
+plt.xlabel('Shortest Path Length')
+plt.ylabel('Number of shortest paths')
+#plt.plot(x, y)
 
 #print(collections.Counter(nx.shortest_path_length(G)))
 
-nx.draw(G, node_size = [3 for v in G], with_labels = True)
+#nx.draw(G, node_size = [3 for v in G], with_labels = True)
 plt.show()
