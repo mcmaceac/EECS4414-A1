23e1e184c5cda624c6c1dfc739d3274f2bff0f2d
Matthew MacEachern <mcm.maceachern@gmail.com>
2018-02-20 02:10:25 -0500

    Clustering coefficient plot implemented

--- er.py
+++ er.py
@@ -3,14 +3,20 @@ import matplotlib.pyplot as plot
 import sys 
 import re
 
-n = 10000
+n = 1000
 p = 1/6
-
 G = nx.erdos_renyi_graph(n, p)
 
 i = nx.degree_histogram(G)
-
 plot.ylabel('Node Degree')
 plot.xlabel('Frequency')
 plot.plot(i)
+plot.show()
+
+clust = nx.clustering(G)
+x = list(clust.keys())
+y = list(clust.values())
+plot.ylabel('Clustering Coefficient')
+plot.xlabel('Node Number')
+plot.plot(x, y)
 plot.show()
