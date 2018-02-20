5b9995aea1bc1588a60b803bc1d386f9a5920ff8
Matthew MacEachern <mcm.maceachern@gmail.com>
2018-02-20 14:59:18 -0500

    extracting path length values implemented

--- test.py
+++ test.py
@@ -2,12 +2,24 @@ import networkx as nx
 import matplotlib.pyplot as plt 
 import sys 
 import re
+import collections
 
-n = 100
+def extract_lengths(dict):
+	result = []
+	for dictionary in dict:
+		for length in dictionary.values():
+			result.append(length)
+	return result
+
+n = 5
 p = 1/6
 G = nx.erdos_renyi_graph(n, p)
 
-#print(nx.all_pairs_shortest_path_length(G))
+shortestPathDicts = list(nx.shortest_path_length(G).values())
+print(shortestPathDicts)
+print(extract_lengths(shortestPathDicts))
 
-nx.draw(G)
-plt.show
+#print(collections.Counter(nx.shortest_path_length(G)))
+
+nx.draw(G, node_size = [3 for v in G], with_labels = True)
+plt.show()
