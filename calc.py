ea8c66bc1451f2b73a452e9b30e53f05c00c019b
Matthew MacEachern <mcm.maceachern@gmail.com>
2018-02-21 00:54:04 -0500

    Changed scripts to auto-generate figures and data
    The scripts are going to be running for an extended period of time (15
    10,000 node graphs to be generated and analyzed). Therefore, the saving
    of the figures and data needs to be automated so i can run the script
    overnight.

--- calc.py
+++ calc.py
@@ -16,15 +16,24 @@ def extract_length_distribution(dict):
 	return collections.Counter(result)
 
 
-def computeGraphMeasurements(G):
+def computeGraphMeasurements(G, fileName):
 
+	file = open(fileName + "/" + fileName + "_data.txt", "w")
+	
+	#storing the adjacency list of the generated graph for later use
+	fh = open(fileName + "/" + fileName + ".adjlist", "wb")
+	nx.write_adjlist(G, fh)
+	fh.close()
+	
 	#B part i
 	i = nx.degree_histogram(G)		#A list of frequencies of degrees. The degree values are the index in the list.
 	plot.ylabel('Frequency')
 	plot.xlabel('Node degree')
 	plot.plot(i)
-	plot.show()
+	#plot.show()			#only needed for on demand testing
+	plot.savefig(fileName + "/" + fileName + "_degree_dist.png")
 
+	
 	#B part ii
 	clust = nx.clustering(G)
 	x = list(clust.keys())
@@ -33,10 +42,12 @@ def computeGraphMeasurements(G):
 	plot.xlabel('Node Number')
 	x_pos = np.arange(len(x))
 	plot.bar(x_pos, y, align='center', alpha=0.5)
-	plot.show()
+	#plot.show()
+	plot.savefig(fileName + "/" + fileName + "_clust_dist.png")
 
 	#B part iii
-	print("Global clustering coefficient: %s" % nx.average_clustering(G))
+	#print("Global clustering coefficient: %s" % nx.average_clustering(G))
+	file.write("Global clustering coefficient: %s\n" % nx.average_clustering(G))
 
 	#B part iv
 	shortestPathDicts = list(nx.shortest_path_length(G).values())
@@ -48,10 +59,14 @@ def computeGraphMeasurements(G):
 	x_pos = np.arange(len(x))
 	plot.bar(x_pos, y, align='center', alpha=0.5)
 	plot.xticks(x_pos, x)
-	plot.show()
+	#plot.show()
+	plot.savefig(fileName + "/" + fileName + "_spl_dist.png")
 
 	#B part v
-	print("Average shortest path length: %s" % nx.average_shortest_path_length(G))
+	#print("Average shortest path length: %s" % nx.average_shortest_path_length(G))
+	file.write("Average shortest path length: %s\n" % nx.average_shortest_path_length(G))
 
 	#B part vi
-	print("Diameter of the graph: %s" % nx.diameter(G))
+	#print("Diameter of the graph: %s" % nx.diameter(G))
+	file.write("Diameter of the graph: %s\n" % nx.diameter(G))
+	file.close()
--- main.py
+++ main.py
@@ -1,25 +1,102 @@
 import networkx as nx 
 import matplotlib.pyplot as plot 
+from calc import computeGraphMeasurements
 
 def showGraph(G):
 	#show the graph itself
-	nx.draw(G, node_size = [3 for v in G])
+	nx.draw(G, node_size = [1 for v in G])
 	plot.show()
+	
+def erdos_graphs():
+	n = 10000
+	p = 1/6
+	fileName = "er1"
+	G1 = nx.erdos_renyi_graph(n, p)
+	computeGraphMeasurements(G1, fileName)
+	
+	p = 1/10
+	fileName = "er2"
+	G2 = nx.erdos_renyi_graph(n, p)
+	computeGraphMeasurements(G2, fileName)
+	
+	p = 1/16
+	fileName = "er3"
+	G3 = nx.erdos_renyi_graph(n, p)
+	computeGraphMeasurements(G3, fileName)
+	
+	p = 1/5
+	fileName = "er4"
+	G4 = nx.erdos_renyi_graph(n, p)
+	computeGraphMeasurements(G4, fileName)
+	
+	p = 1/50
+	fileName = "er5"
+	G5 = nx.erdos_renyi_graph(n, p)
+	computeGraphMeasurements(G5, fileName)
+	
+def watts_graphs():
+	n = 10000
+	k = 4		#Each node is connected to k nearest neighbors in ring topology
+	p = 1/6		#Each node is connected to k nearest neighbours with probability p to rewire
+	fileName = "wa1"
+	G1 = nx.watts_strogatz_graph(n, k, p)
+	computeGraphMeasurements(G1, fileName)
+	
+	p = 1/10
+	k = 2
+	fileName = "wa2"
+	G2 = nx.watts_strogatz_graph(n, k, p)
+	computeGraphMeasurements(G2, fileName)
+	
+	p = 1/16
+	k = 1
+	fileName = "wa3"
+	G3 = nx.watts_strogatz_graph(n, k, p)
+	computeGraphMeasurements(G3, fileName)
+	
+	p = 1/5
+	k = 10
+	fileName = "wa4"
+	G4 = nx.watts_strogatz_graph(n, k, p)
+	computeGraphMeasurements(G4, fileName)
+	
+	p = 1/50
+	k = 20
+	fileName = "wa5"
+	G5 = nx.watts_strogatz_graph(n, k, p)
+	computeGraphMeasurements(G5, fileName)
+	
+def barabasi_graphs():
+	n = 10000
 
-from calc import computeGraphMeasurements
+	m = 5 	#Number of edges to attach from a new node to existing nodes
+	fileName = "ba1"
+	G1 = nx.watts_strogatz_graph(n, m)
+	computeGraphMeasurements(G1, fileName)
+	
+	m = 10
+	fileName = "ba2"
+	G2 = nx.watts_strogatz_graph(n, m)
+	computeGraphMeasurements(G2, fileName)
+	
+	m = 20
+	fileName = "ba3"
+	G3 = nx.watts_strogatz_graph(n, m)
+	computeGraphMeasurements(G3, fileName)
+	
+	m = 50
+	fileName = "ba4"
+	G4 = nx.watts_strogatz_graph(n, m)
+	computeGraphMeasurements(G4, fileName)
 
-n = 1000		#Number of nodes
-p = 1/500
-k = 4		#Each node is connected to k nearest neighbors in ring topology
-m = 4		#Number of edges to attach from a new node to existing nodes
+	m = 2
+	fileName = "ba5"
+	G5 = nx.watts_strogatz_graph(n, m)
+	computeGraphMeasurements(G5, fileName)
+	
 
-G = nx.erdos_renyi_graph(n, p)
-
-#Each node is connected to k nearest neighbours with probability p to rewire
-#G = nx.watts_strogatz_graph(n, k, p)
-
-#G = nx.barabasi_albert_graph(n, m)
-
-showGraph(G)
-computeGraphMeasurements(G)
+	
+erdos_graphs()
+watts_graphs()
+barabasi_graphs()
 
