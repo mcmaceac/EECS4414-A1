54a9f857b24472fc8a0a22fcc7d551cf832fbb4a
Matthew MacEachern <mcm.maceachern@gmail.com>
2018-02-23 05:07:37 -0500

    part C implemented (just need to run it on 3 graphs

--- NetworkX/community.py
+++ NetworkX/community.py
@@ -1,16 +1,18 @@
 #https://networkx.github.io/documentation/latest/reference/algorithms/generated/networkx.algorithms.community.centrality.girvan_newman.html
 import itertools
 import networkx as nx
+import matplotlib.pyplot as plot 
 from networkx.algorithms.community.centrality import girvan_newman
 
 def detectCommunities(G):
-	k = 2	#stop after reaching greater than 10 communities
+	communitySizes = []
+	k = 10	#stop after reaching greater than 10 communities
 	comp = girvan_newman(G)
 	limited = itertools.takewhile(lambda c: len(c) <= k, comp)
 	for communities in limited:
-		print(tuple(sorted(c) for c in communities))
-
-n = 100
-p = 1/6
-G = nx.erdos_renyi_graph(n, p)
-detectCommunities(G)
+		if len(communities) == k:	
+			#print(communities)
+			for community in communities:
+				#print(len(community))
+				communitySizes.append(len(community))
+	return sorted(communitySizes, reverse=True)	
