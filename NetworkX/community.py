dcc50ca3ee1eaf92d8e4b1cff993b7c039c5b0d2
Matthew MacEachern <mcm.maceachern@gmail.com>
2018-02-23 13:46:34 -0500

    updated community for automated running for 3 graphs

--- NetworkX/community.py
+++ NetworkX/community.py
@@ -4,7 +4,7 @@ import networkx as nx
 import matplotlib.pyplot as plot 
 from networkx.algorithms.community.centrality import girvan_newman
 
-def detectCommunities(G):
+def detectCommunities(G, fileName):
 	communitySizes = []
 	k = 10	#stop after reaching greater than 10 communities
 	comp = girvan_newman(G)
@@ -15,4 +15,30 @@ def detectCommunities(G):
 			for community in communities:
 				#print(len(community))
 				communitySizes.append(len(community))
-	return sorted(communitySizes, reverse=True)	
+	saveCommunities(sorted(communitySizes, reverse=True), fileName)
+
+def saveCommunities(communityLengths, fileName):
+	file = open(fileName + "/" + fileName + "_communities.txt", "w")
+	file.write(communityLengths)
+	file.close()
+
+fileName = "er1"
+fh = open(fileName + "/" + fileName + ".adjlist", "rb")
+G = nx.read_adjlist(fh)
+print(fileName + " loaded")
+fh.close()
+detectCommunities(G, fileName)
+
+fileName = "ws1"
+fh = open(fileName + "/" + fileName + ".adjlist", "rb")
+G = nx.read_adjlist(fh)
+print(fileName + " loaded")
+fh.close()
+detectCommunities(G, fileName)
+
+fileName = "ba1"
+fh = open(fileName + "/" + fileName + ".adjlist", "rb")
+G = nx.read_adjlist(fh)
+print(fileName + " loaded")
+fh.close()
+detectCommunities(G, fileName)
