71770123f9a85c8fb6e222151c61aecfb91c6e45
Matthew MacEachern <mcm.maceachern@gmail.com>
2018-02-23 04:30:51 -0500

    small change to properly import girvan_newman

--- NetworkX/community.py
+++ NetworkX/community.py
@@ -1,10 +1,16 @@
 #https://networkx.github.io/documentation/latest/reference/algorithms/generated/networkx.algorithms.community.centrality.girvan_newman.html
 import itertools
 import networkx as nx
+from networkx.algorithms.community.centrality import girvan_newman
 
 def detectCommunities(G):
-	k = 10	#stop after reaching greater than 10 communities
+	k = 2	#stop after reaching greater than 10 communities
 	comp = girvan_newman(G)
 	limited = itertools.takewhile(lambda c: len(c) <= k, comp)
 	for communities in limited:
 		print(tuple(sorted(c) for c in communities))
+
+n = 100
+p = 1/6
+G = nx.erdos_renyi_graph(n, p)
+detectCommunities(G)
